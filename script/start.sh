3fc43874b5beb4b8451d68123ffbca52252f967a
Tophe <christophe@amory.fr>
2017-01-28 19:13:53 +0100

    some refactoring on start up script

--- script/start.sh
+++ script/start.sh
@@ -5,8 +5,12 @@
 EXPOSED_DIRS="ccnet conf logs seafile-data seahub-data"
 EXPOSED_ROOT_DIR=${EXPOSED_ROOT_DIR:-"/seafile"}
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 setup_seafile(){
-	echo "Setting up seafile server ..."
+	log_info "Configuring seafile server"
 	check_require "SEAFILE_ADMIN" $SEAFILE_ADMIN
 	check_require "SEAFILE_ADMIN_PASSWORD" $SEAFILE_ADMIN_PASSWORD
 
@@ -23,16 +27,16 @@ setup_seafile(){
 	    -q "%" \
 	    -c "${CCNET_DB:-"ccnet-db"}" \
 	    -s "${SEAFILE_DB:-"seafile-db"}" \
-	    -b "${SEAHUB_DB:-"seahub-db"}" \
+	    -b "${SEAHUB_DB:-"seahub-db"}"
 
-	echo "Setting up seahub ..."
-  setup_seahub
-
-  echo "Create exposed directories ..."
+	log_info "Seafile server is successfully configured"
+	setup_seahub
   setup_exposed_directories
+	link_exposed_directories
 }
 
 setup_seahub(){
+	log_info "Configuring seahub server"
 	# From https://github.com/haiwen/seafile-server-installer-cn/blob/master/seafile-server-ubuntu-14-04-amd64-http
 	sed -i 's/= ask_admin_email()/= '"\"${SEAFILE_ADMIN}\""'/' ${SERVER_DIR}/check_init_admin.py
 	sed -i 's/= ask_admin_password()/= '"\"${SEAFILE_ADMIN_PASSWORD}\""'/' ${SERVER_DIR}/check_init_admin.py
@@ -44,41 +48,45 @@ setup_seahub(){
  	control_seahub "stop"
 	sleep 1
 	control_seafile "stop"
+	log_info "Seahub server is successfully configured"
 }
 
 wait_for_db() {
-	echo "Waiting for the DB server is up ..."
+	log_info "Trying to connect to the DB server"
 	DOCKERIZE_TIMEOUT=${DOCKERIZE_TIMEOUT:-"60s"}
 	dockerize -timeout ${DOCKERIZE_TIMEOUT} -wait tcp://${MYSQL_SERVER}:${MYSQL_PORT:-3306}
 	if [[ $? -ne 0 ]]; then
-		echo "Cannot connect to the DB server"
+		log_error "Cannot connect to the DB server"
 		exit 1
 	fi
-	echo "DB server is OK"
+	log_info "Successfully connected to the DB server"
 }
 
 check_required_params() {
-	echo "Checking required params..."
+	log_info "Checking required params"
 	check_require "MYSQL_SERVER" $MYSQL_SERVER
 	check_require "MYSQL_ROOT_PASSWORD" $MYSQL_ROOT_PASSWORD
 	check_require "MYSQL_USER" $MYSQL_USER
 	check_require "MYSQL_USER_PASSWORD" $MYSQL_USER_PASSWORD
-	echo "Required params OK"
 }
 
 setup_exposed_directories() {
+	log_info "Creating exposed directories"
 	for EXPOSED_DIR in $EXPOSED_DIRS
 	do
-		if [[ -e $SEAFILE_ROOT_DIR/$EXPOSED_DIR ]]; then
-			if [[ ! -L  $SEAFILE_ROOT_DIR/$EXPOSED_DIR ]]; then
+		if [[ -d $SEAFILE_ROOT_DIR/$EXPOSED_DIR ]]; then
 				mv "$SEAFILE_ROOT_DIR/$EXPOSED_DIR" "$EXPOSED_ROOT_DIR"
-				ln -sf "$EXPOSED_ROOT_DIR/$EXPOSED_DIR" "$SEAFILE_ROOT_DIR/$EXPOSED_DIR"
-			fi
 		else
 			mkdir "$EXPOSED_ROOT_DIR/$EXPOSED_DIR"
-			ln -sf "$EXPOSED_ROOT_DIR/$EXPOSED_DIR" "$SEAFILE_ROOT_DIR/$EXPOSED_DIR"
 		fi
 	done
+}
+
+link_exposed_directories() {
+	for EXPOSED_DIR in $EXPOSED_DIRS
+  do
+    ln -sf "$EXPOSED_ROOT_DIR/$EXPOSED_DIR" "$SEAFILE_ROOT_DIR/$EXPOSED_DIR"
+  done
 }
 
 is_new_install(){
@@ -93,18 +101,15 @@ is_new_install(){
 		fi
 	done
 	if [[ $DIR_COUNTER -gt 0 && $DIR_COUNTER -lt $(wc -w <<< "$EXPOSED_DIRS") ]]; then
-		echo "Inconsistent state. Following directories are missing to restore previous install: $MISSING_DIR"
+		log_error "Inconsistent state. Following directories are missing to restore previous install: $MISSING_DIR"
     exit 1
 	fi
 	return $DIR_COUNTER
 }
 
 restore_install(){
-	echo "Restoring previous install"
-  for EXPOSED_DIR in $EXPOSED_DIRS
-  do
-    ln -sf "$EXPOSED_ROOT_DIR/$EXPOSED_DIR" "$SEAFILE_ROOT_DIR/$EXPOSED_DIR"
-  done
+	log_info "Restoring previous install"
+	link_exposed_directories
 	ln -sf "$SERVER_DIR" "$LATEST_SERVER_DIR"
 }
 
@@ -116,9 +121,18 @@ control_seahub() {
 	"$SERVER_DIR"/seahub.sh "$@"
 }
 
+log_info() {
+	printf "$GREEN[$(date +"%F %T,%3N")] $1$NC\n"
+}
+
+log_error() {
+	printf "$RED[$(date +"%F %T,%3N")] $1$NC\n"
+}
+
+
 check_require() {
 	if [[ -z ${2//[[:blank:]]/} ]]; then
-		echo "$1 is required"
+		log_error "$1 is required"
 		exit 1
 	fi
 }
@@ -131,7 +145,8 @@ if [[ ! -e $LATEST_SERVER_DIR ]]; then
 	else
 	  restore_install
 	fi
-	echo "Done. Starting seafile"
+	log_info "All is Done"
+	log_info "Starting Seafile ..."
 fi
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
