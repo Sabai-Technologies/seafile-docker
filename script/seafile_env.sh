d94fa0ac6956fe0d488e38ee48572a3664e91029
Tophe <christophe@amory.fr>
2017-01-28 19:48:35 +0100

    improved Dockerfile

--- script/seafile_env.sh
+++ script/seafile_env.sh
@@ -1,4 +1,3 @@
-export SEAFILE_ROOT_DIR=${SEAFILE_ROOT_DIR:-"/opt/seafile"}
 export SERVER_DIR_BASENAME="seafile-server"
 export LATEST_SERVER_DIR=${SEAFILE_ROOT_DIR}/${SERVER_DIR_BASENAME}-latest
 export SERVER_DIR="$SEAFILE_ROOT_DIR"/$(ls -1 "$SEAFILE_ROOT_DIR" | grep -E "$SERVER_DIR_BASENAME-[0-9.-]+")
